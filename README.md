797e862d1c3a5aa9638ceccc36d3f9a77c7e7852
Sean Zammit <smez12345@gmail.com>
2020-02-18 23:45:24 +1100

    Release 3.0.2

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Arachne 3
 =================
-RELEASE VERSION: Arachne 3.0.1 (2020)
+RELEASE VERSION: Arachne 3.0.2 (2020)
--- src/main/java/arachne/lib/AutoManager.java
+++ src/main/java/arachne/lib/AutoManager.java
@@ -1,27 +1,29 @@
 package arachne.lib;
 
+import java.util.function.Function;
+
 import arachne.lib.scheduler.Schedulable;
 import arachne.lib.sequences.ActionConductor;
 import arachne.lib.sequences.Actionable;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
-public class AutoManager<AutoT extends Enum<?> & Actionable> implements Schedulable
+public class AutoManager<RobotT, AutoT extends Enum<?> & Function<RobotT, Actionable>> implements Schedulable
 {
 	protected static final String DASHBOARD_TAB = "Autonomous";
 	protected static final String SELECTION_KEY = "Auto selection";
 	
 	protected final ActionConductor conductor;
-	protected final SendableChooser<AutoT> autoChooser;
+	protected final SendableChooser<Actionable> autoChooser;
 	
-	public AutoManager(AutoT defaultAuto, AutoT[] autos) {
+	public AutoManager(RobotT robot, AutoT defaultAuto, AutoT[] autos) {
 		this.conductor = new ActionConductor();
 		
-		this.autoChooser = new SendableChooser<AutoT>();
-		this.autoChooser.setDefaultOption(defaultAuto.toString(), defaultAuto);
+		this.autoChooser = new SendableChooser<Actionable>();
+		this.autoChooser.setDefaultOption(defaultAuto.toString(), defaultAuto.apply(robot));
 		
 		for(AutoT auto : autos) {
-			if(auto != defaultAuto) autoChooser.addOption(auto.toString(), auto);
+			if(auto != defaultAuto) autoChooser.addOption(auto.toString(), auto.apply(robot));
 		}
 
 		Shuffleboard.getTab(DASHBOARD_TAB).add(SELECTION_KEY, autoChooser);
@@ -31,7 +33,7 @@ public class AutoManager<AutoT extends Enum<?> & Actionable> implements Schedula
 		startAuto(autoChooser.getSelected());
 	}
 	
-	public void startAuto(AutoT auto) {
+	public void startAuto(Actionable auto) {
 		stopAuto();
 		conductor.add(auto);
 	}
